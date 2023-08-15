add0b38c0c13da8d3ef84eb4f4c323c92c2744f8
ToshiKajitsu <toshikajitsu@gmail.com>
2023-08-15 19:59:59 +0400

    add work card info

--- src/features/workCard/workCard.jsx
+++ src/features/workCard/workCard.jsx
@@ -3,5 +3,20 @@ import './workCard.css';
 
 export const WorkCard = ({data}) => {
 
-    return <p>{data.siteName}</p>
+    return <div>
+                <div className='wrapper'>
+                    <div className='workName'>
+                        <h4>{data.siteName}</h4>
+                        <div className="dateAndLvl">
+                            <p>{data.grade}</p>
+                            <p>{data.data}</p>
+                        </div>
+                        </div>
+                        <div className='workInfo'>
+                        <h4>{data.work}</h4>
+                        <p>{data.workHistory}</p>
+                    </div>
+                </div>
+                <div className="line"></div>
+        </div>
 }
