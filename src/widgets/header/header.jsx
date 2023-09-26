28c139736afdb162b95edf3f44257990639d5778
ToshiKajitsu <toshikajitsu@gmail.com>
2023-09-26 20:38:26 +0400

    header container

--- src/widgets/header/header.jsx
+++ src/widgets/header/header.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import './header.css';
 import { Element } from 'react-scroll';
+import { Paragraph } from '../../shared/paragraph/paragraph';
+import YourImage  from './yourImage.svg'
 
 
 export const Header = () => {
     return (
-        <Element name='home' className="header">Header</Element>
+        <Element name='home' className="header">
+            <div className="mainHeader">
+                <h1>I’m Rayan Adlrdard <span>Front-end</span> Developer</h1>
+                <Paragraph text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et, volutpat feugiat placerat lobortis. Natoque rutrum semper sed suspendisse nunc lectus.' />
+                <button>HIRE ME</button>
+            </div>
+            <img src={YourImage} alt="img" />
+        </Element>
     )
 }
